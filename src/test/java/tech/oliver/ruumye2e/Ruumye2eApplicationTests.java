b612e7dd17e303f12c5bfde17ee85e3526e50f32
OliverBR1 <Br.0liveira@outlook.com>
2026-09-20 16:51:46 -0300

    first commit

---
+++ src/test/java/tech/oliver/ruumye2e/Ruumye2eApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.oliver.ruumye2e;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ruumye2eApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
