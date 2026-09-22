661ce5847253f30ffb19be42d4ac728a9e52821f
OliverBR1 <Br.0liveira@outlook.com>
2026-09-22 11:44:37 -0300

    create RoomStep/RoomDto

---
+++ src/test/java/tech/oliver/ruumye2e/dto/RoomDto.java
@@ -0,0 +1,4 @@
+package tech.oliver.ruumye2e.dto;
+
+public record RoomDto(Long id, String name) {
+}
---
+++ src/test/java/tech/oliver/ruumye2e/stepdefinitions/RoomStepTest.java
@@ -0,0 +1,46 @@
+package tech.oliver.ruumye2e.stepdefinitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.common.mapper.TypeRef;
+import io.restassured.response.Response;
+import org.springframework.http.MediaType;
+import tech.oliver.ruumye2e.config.RestConfig;
+import tech.oliver.ruumye2e.config.ScenarioContext;
+import tech.oliver.ruumye2e.dto.RoomDto;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RoomStepTest {
+
+    private final ScenarioContext scenarioContext;
+    private final RestConfig restConfig;
+
+    public RoomStepTest(ScenarioContext scenarioContext, RestConfig restConfig) {
+        this.scenarioContext = scenarioContext;
+        this.restConfig = restConfig;
+    }
+
+    @When("I list all the rooms")
+    public void iListAllTheRooms() {
+        var response = restConfig.givenBackEnd()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .get("/rooms");
+
+        scenarioContext.put("response", response);
+    }
+
+    @Then("I detect that the room {string} exists")
+    public void iDetectThatTheRoomNameExists(String name) {
+        var response = scenarioContext.get("response", Response.class);
+
+        var rooms = response.then()
+                .statusCode(200)
+                .extract()
+                .as(new TypeRef<List<RoomDto>>() {});
+
+      assertTrue(rooms.stream().anyMatch(r -> r.name().equalsIgnoreCase(name)));
+    }
+}
