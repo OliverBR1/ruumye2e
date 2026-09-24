489401a0c54bd1b40542d142705d22124606de2c
OliverBR1 <br.0liveira@outlook.com>
2026-09-24 01:31:31 -0300

    create BookingRequest

---
+++ src/test/java/tech/oliver/ruumye2e/dto/BookingRequest.java
@@ -0,0 +1,11 @@
+package tech.oliver.ruumye2e.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+
+public record BookingRequest(Long roomId,
+                             @JsonFormat(shape = JsonFormat.Shape.STRING) LocalDateTime startTime,
+                             @JsonFormat(shape = JsonFormat.Shape.STRING) LocalDateTime endTime) {
+
+}
